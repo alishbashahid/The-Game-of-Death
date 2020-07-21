4ea858667378b31e9e6ebfff77dfd3794373a6e4
alishbashahid <alishbashahid9876@gmail.com>
2020-07-22 02:36:38 +0500

    Changed functionality for gracefully initializing entities without collision.

--- src/com/afiniti/the_game_of_death/Covid19Simulation.java
+++ src/com/afiniti/the_game_of_death/Covid19Simulation.java
@@ -1,5 +1,6 @@
 package com.afiniti.the_game_of_death;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Covid19Simulation extends DiseaseSimulation{
@@ -39,12 +40,20 @@ public class Covid19Simulation extends DiseaseSimulation{
     }
 
     public void initializeOffices(){
+        int block_size = getBlockSize(n,o);
+
+        int start_x = 0;
+        int start_y = 0;
+
+        int end_x = block_size;
+        int end_y = block_size;
+
         int temp = 1;
 
         while (temp <= o) {
 
-            int x = random.nextInt(n);
-            int y = random.nextInt(n);
+            int x = start_x+random.nextInt(end_x-start_x+1);
+            int y = start_y+random.nextInt(end_y-start_y+1);
 
             if (offices.containsKey(new Coordinates(x,y))) continue;
 
@@ -53,21 +62,46 @@ public class Covid19Simulation extends DiseaseSimulation{
             offices.put(office.getHomeLocation(),office);
 
             temp++;
+
+            start_x += block_size+1;
+            end_x += block_size;
+
+            if(end_x>=n){
+                start_x = 0;
+                end_x = block_size;
+                start_y += block_size+1;
+                end_y += block_size;
+                if (end_y>=n){
+                    start_x = 0;
+                    end_x = block_size;
+                    start_y = 0;
+                    end_y = block_size;
+                }
+            }
             System.out.println(x+" "+y);
         }
-        System.out.println(offices.size());
+
     }
 
     private void initializeHumans(){
+        int block_size = getBlockSize(n,h);
+
+        int start_x = 0;
+        int start_y = 0;
+
+        int end_x = block_size;
+        int end_y = block_size;
+
         int temp = 1;
+
         Office[] temp_offices = offices.values().toArray(new Office[o]);
 
         while (temp <= w) {
 
-            int x = random.nextInt(n);
-            int y = random.nextInt(n);
+            int x = start_x+random.nextInt(end_x-start_x);
+            int y = start_y+random.nextInt(end_y-start_y);
 
-            if (humans.containsKey(new Coordinates(x,y))) continue;
+            if (humans.containsKey(new Coordinates(x,y)) || offices.containsKey(new Coordinates(x,y))) continue;
 
             //TODO: make offices initially occupied
             WorkingHuman human = new WorkingHuman(Integer.toString(temp), new Coordinates(x,y), generateRandomImmunity(), temp_offices[random.nextInt(o)]);
@@ -75,21 +109,55 @@ public class Covid19Simulation extends DiseaseSimulation{
             humans.put(human.getCurrentLocation(),human);
 
             temp++;
+
+            start_x += block_size+1;
+            end_x += block_size;
+
+            if(end_x>=n){
+                start_x = 0;
+                end_x = block_size;
+                start_y += block_size+1;
+                end_y += block_size;
+                if (end_y>=n){
+                    start_x = 0;
+                    end_x = block_size;
+                    start_y = 0;
+                    end_y = block_size;
+                }
+            }
+
             System.out.println(x+" "+y);
         }
 
         while (temp <= h) {
 
-            int x = random.nextInt(n);
-            int y = random.nextInt(n);
+            int x = start_x+random.nextInt(end_x-start_x);
+            int y = start_y+random.nextInt(end_y-start_y);
 
-            if (humans.containsKey(new Coordinates(x,y))) continue;
+            if (humans.containsKey(new Coordinates(x,y)) || offices.containsKey(new Coordinates(x,y))) continue;
 
             Human human = new Human(Integer.toString(temp), new Coordinates(x,y), generateRandomImmunity());
             habitatEntities.add(human);
             humans.put(human.getCurrentLocation(),human);
 
             temp++;
+
+            start_x += block_size+1;
+            end_x += block_size;
+
+            if(end_x>=n){
+                start_x = 0;
+                end_x = block_size;
+                start_y += block_size+1;
+                end_y += block_size;
+                if (end_y>=n){
+                    start_x = 0;
+                    end_x = block_size;
+                    start_y = 0;
+                    end_y = block_size;
+                }
+            }
+
             System.out.println(x+" "+y);
         }
 
@@ -213,6 +281,30 @@ public class Covid19Simulation extends DiseaseSimulation{
             }
         }
         return false;
+    }
+
+    private int getBlockSize(int n, int block_count)
+    {
+        //divide the area but the number of blocks to get the max area a block could cover
+        //this optimal size for a block will more often than not make the blocks overlap, but
+        //a block can never be bigger than this size
+        BigInteger big_n = new BigInteger(String.valueOf(n));
+        BigInteger big_block = new BigInteger(String.valueOf(n));
+
+        double maxSize = Math.sqrt((big_n.multiply(big_n)).divide(big_block).doubleValue());
+        //find the number of whole blocks that can fit into the height
+        double possibleBlocksinN = Math.floor(n / maxSize);
+        //works out how many whole blocks this configuration can hold
+        double total = possibleBlocksinN * possibleBlocksinN;
+
+        //if the number of number of whole blocks that the max size block ends up with is less than the require number of
+        //blocks, make the maxSize smaller and recaluate
+        while(total < block_count){
+            maxSize--;
+            possibleBlocksinN = Math.floor(n / maxSize);
+            total = possibleBlocksinN * possibleBlocksinN;
+        }
+        return (int)Math.floor(maxSize);
     }
 
     private float generateRandomImmunity(){
